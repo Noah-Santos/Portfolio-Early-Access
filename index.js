7ed70575d61a7400c03fd5e09b08a71f528643dd
Noah-Santos <nsanto591@west-mec.org>
2022-12-09 16:04:53 -0700

    Disappearing
    Got sections to appear and disappear.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function showDiv(text){
     document.getElementById(text).style.display="flex";
-    console.log("change")
+    document.getElementById(text).style.alignItems="stretch";
+    document.getElementById(text).style.zIndex=1;
+    console.log("change");
+}
+function testing(){
+    console.log("success");
+}
+function closePage(text){
+    document.getElementById(text).style.display="none";
+    console.log("success");
 }
